b7114f6a4f49bd61f919e8050c0c08408ea85d07
Frank Zalkow <frank_zalkow@web.de>
2015-01-30 23:21:51 +0100

    adjusted docs to new structure

--- api/generate-api.lisp
+++ api/generate-api.lisp
@@ -1,19 +1,11 @@
 ;;;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; Base: 10 -*-
 
-(unless (find-package :atdoc)
-  (if (find-package :quicklisp)
-      (ql:quickload :atdoc)
-    (error "ATDOC has to be loaded for generating the API documentation. QUICKLISP isn't available either.")))
+(unless (find-package :quicklisp)
+  (error "You must have Quicklisp!"))
 
+(ql:quickload :atdoc)
+(ql:quickload :mlep-add)
 
-
-(let ((mlep-load-file (make-pathname :directory (butlast (pathname-directory *load-truename*) 2)
-                                     :name "load-with-add"
-                                     :type "lisp")))
-  (if (probe-file mlep-load-file)
-      (load mlep-load-file)
-	  (error "Cannot find load-file for CL-MLEP.")))
-        
 (atdoc:generate-html-documentation
  '(:mlep :mlep-add)
  (make-pathname :directory (pathname-directory *load-truename*))
@@ -23,5 +15,3 @@
  :include-internal-symbols-p nil
  :include-slot-definitions-p t
  :css (make-pathname :directory (pathname-directory *load-truename*) :name "api" :type "css"))
-
-
